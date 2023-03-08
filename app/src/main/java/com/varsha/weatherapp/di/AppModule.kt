1fed244dd6d14782192c6664db139c52376a90f1
varsha <vbhatia_2009@ymail.com>
2023-03-08 12:45:46 +0000

    Added weather api,converter and DTOs

---
+++ app/src/main/java/com/varsha/weatherapp/di/AppModule.kt
@@ -0,0 +1,25 @@
+package com.varsha.weatherapp.di
+
+import com.varsha.weatherapp.data.remote.WeatherApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+    @Provides
+    @Singleton
+    fun provideWeatherApi(): WeatherApi {
+        return Retrofit.Builder()
+            .baseUrl("https://api.open-meteo.com/")
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+            .create()
+    }
+}
